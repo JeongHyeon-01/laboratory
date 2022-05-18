3e55bba8c183ed31b5cacad09e15a6021d40d893
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-05-18 20:00:19 +0900

    [ADD]Question_Model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import Users
+from utils.models import TimeStampModel
 
-# Create your models here.
+class Questions(TimeStampModel):
+    title = models.CharField(max_length=45),
+    content = models.TextField()
+    user = models.ForeignKey(Users,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'question'
+
+class Comments(TimeStampModel):
+    question = models.ForeignKey('Questions', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.Users',on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'comment'
+
+class Likes(models.Model):
+    question = models.ForeignKey('Questions', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.Users',on_delete=models.CASCADE)
+    like = models.BooleanField()
+
+    class Meta:
+        db_table = 'like'
+
